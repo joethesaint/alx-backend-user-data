62f5e46ffa0be96ff33edf189a63c4f2f939cbf5
joethesaint <joebamisaye068@gmail.com>
2023-05-03 01:08:03 +0000

    refactor[index.py]: add new endpoint

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def new_error() -> str:
+    """
+    By calling abort(401), the error
+    handler will be executed.
+    """
+    return abort(401, description="Unauthorised")
